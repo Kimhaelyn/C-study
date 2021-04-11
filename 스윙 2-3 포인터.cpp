e1707ea57c4bc5b68fc5ffc74b076fe10d1778d5
Kimhaelyn <81563342+Kimhaelyn@users.noreply.github.com>
2021-04-12 00:40:26 +0900

    Add files via upload

---
+++ 스윙 2-3 포인터.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 10;
+	int* ptr; //변수 앞에 *을 붙여 포인터 선언하기
+	ptr = &a; //변수 앞에 &을 붙여 포인터 초기화 하기
+
+	cout << "변수 a의 주소는 :" << ptr << endl;
+	cout << "a의 값은: " << a << endl;
+	return 0;
+}
